a69a23d125ba2df6df3b5b2bf511f918dab8e38a
Rajan <themailboxofabi@yahoo.com>
2025-03-09 23:42:25 +0530

    Timer and Coin count functions
    StringBuilder and append are used for the timer and the coin count.

--- Roller Baller/Assets/Scripts/Gameplay Scripts/GameplayManager.cs
+++ Roller Baller/Assets/Scripts/Gameplay Scripts/GameplayManager.cs
@@ -19,7 +19,9 @@ public class GameplayManager : MonoBehaviour
     private float timerCount;
     private StringBuilder coinString = new StringBuilder();
 
-    private StringBuilder timerString = new StringBuilder(); //to use a string builder we have to declare it
+    private StringBuilder timerString = new StringBuilder(); //to use a string builder we have to declare it. More efficient when adding strings instead of concatenating
+
+    //string builder is an array for storing strings
 
     private bool gameOver;
 
@@ -30,12 +32,56 @@ public class GameplayManager : MonoBehaviour
     {
         if (instance != null)
         {
+            Destroy(gameObject); // Destroy the copy (current copy) holding the copy of the gameplay manager
 
         }
 
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        timerCount = Time.time + 1f; //advance time by 1 before we start counting the time
+
     }
 
+    public void SetCoinCount(int coinValue) // we can use the coinValue parameter to add or subtract
+    {
+        coinCount += coinValue;
+        coinString.Length = 0; // explained in line 68
+        coinString.Append("Coins: ");
+        coinTxt.text = coinString.ToString(); //creates only 1 string
 
+        // this is much more efficient than what is below.. what is below creates 2 strings
+        // counTxt.text= "Coin: " + coinCount.ToString(); 
+
+    }
+
+       void CountTimer()
+    {
+        if(Time.time >timerCount)
+        {
+            timerCount = Time.time+1;
+            timerThreshold--;
+
+            timerString.Length = 0; // if you omit this, .. assume that time is "Time: 149" the next time you append the time(next line), "Time: 149Time: 148"
+            timerString.Append("Time :");
+            timerString.Append(timerThreshold.ToString());
+
+            timerTxt.text = timerString.ToString();
+        }
+
+
+
+    }
+
+    public void GameOver()
+    {
+        gameOver = true; // will control update function with it
+
+        GameObject.FindWithTag(TagManager.PLAYER_TAG).GetComponent<BallController>().DestroyPlayer();
+    }
 
 
 
